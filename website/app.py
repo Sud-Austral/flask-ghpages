e0e2b5f72eab6801613ff581138885eb6c9f9ca7
lmonsalve22 <lmonsalve22@gmail.com>
2023-08-21 17:30:27 -0400

    Update app.py

--- website/app.py
+++ website/app.py
@@ -7,8 +7,18 @@ def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('settings.py')
 
-    @app.route("/<name>")
-    def index(name):
+    @app.route("/")
+    def index():
+        #return "hello world"
+        data = {
+        "message": "Hello, world!",
+        "status": "success",
+        }
+        return jsonify(data)
+    
+    @app.route("/greed")
+    @app.route("/greed/<name>")
+    def greed(name="stranger"):
         #return "hello world"
         data = {
         "message": "Hello, world!",
