aa07738c901029625b5a1dfd09979f5060c5114a
lmonsalve22 <lmonsalve22@gmail.com>
2023-08-21 17:20:22 -0400

    segundo intento

--- website/app.py
+++ website/app.py
@@ -1,14 +1,19 @@
-import flask
+from flask import Flask, jsonify
 
 __all__ = ["create_app"]
 
 
 def create_app():
-    app = flask.Flask(__name__)
+    app = Flask.Flask(__name__)
     app.config.from_pyfile('settings.py')
 
     @app.route("/")
     def index():
-        return "hello world"
+        #return "hello world"
+        data = {
+        "message": "Hello, world!",
+        "status": "success"
+        }
+        return jsonify(data)
 
     return app
